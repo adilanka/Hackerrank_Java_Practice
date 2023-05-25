ea647ea6e5742f687b7503e6383ef970d45d2540
Aditya Lanka <87301182+adilanka@users.noreply.github.com>
2023-05-25 22:33:43 +0530

    Add files via upload

---
+++ Eulers.java
@@ -0,0 +1,30 @@
+
+public class Eulers {
+
+	public static void main(String[] args) 
+	{
+		int n;
+		//System.out.println(phi(n));
+		for(n=1;n<=10;n++)
+		{
+			System.out.println("phi("+n+") = "+phi(n));
+		}
+	}
+	static int gcd(int a, int b)
+	{
+		if(a==0)
+			return b;
+		return gcd(b%a,a);
+	}
+	static int phi(int n)
+	{
+		int result = 1;
+		for(int i = 2;i<n;i++)
+		{
+			if(gcd(i,n)==1)
+				result++;
+		}
+		return result;
+	}
+
+}
