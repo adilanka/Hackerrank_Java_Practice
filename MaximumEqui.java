e4ad34428d548ca88def0ffcee9cd5375467e032
Aditya Lanka <87301182+adilanka@users.noreply.github.com>
2023-05-27 21:28:23 +0530

    Add files via upload

---
+++ MaximumEqui.java
@@ -0,0 +1,23 @@
+
+public class MaximumEqui {
+	
+	public static void main(String args[])
+	{
+		int arr[] = {-1,2,9,0,9,2,-1};
+		int n = arr.length;
+		
+	}
+	
+	int maxEqui(int arr[], int n)
+	{
+		int presum[] = {};
+		int suffsum[] = {};
+		int maxSum = 0;
+		
+		for(int i = 0; i<=n;i++)
+		{
+			maxSum += 
+			
+		}
+	}
+}
