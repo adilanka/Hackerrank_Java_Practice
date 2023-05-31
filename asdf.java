c7c254524c049ee2a55d287b0be40bff8072f050
Aditya Lanka <87301182+adilanka@users.noreply.github.com>
2023-05-31 22:29:27 +0530

    Add files via upload

---
+++ asdf.java
@@ -0,0 +1,14 @@
+
+public class asdf {
+	public static void main(String args[])
+	{
+		int k = 10;
+		int count=0;
+		for(int i = 0;i<k;i=i+2)
+		{
+			k = k+i;
+			count++;
+		}
+		System.out.println(count);
+	}
+}
